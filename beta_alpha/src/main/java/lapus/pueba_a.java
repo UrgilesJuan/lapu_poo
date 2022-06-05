c1d730683338c8aa2643ed693261e211df02846b
JAUT <jaurgile@espol.edu.ec>
2022-06-05 18:04:53 -0500

    aumento clase

---
+++ beta_alpha/src/main/java/lapus/pueba_a.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lapus;
+import java.util.Scanner;
+/**
+ *
+ * @author jaut2
+ */
+public class pueba_a {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese nombre: ");
+        String nombre=sc.nextLine();
+        System.out.println("el nombre es: "+nombre);
+//que pasa
+    }
+}
