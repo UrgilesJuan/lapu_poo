01dfeeabe2b2f07fc0bf51611d3c067da4766b48
JAUT <jaurgile@espol.edu.ec>
2022-06-06 19:47:02 -0500

    A

---
+++ beta_alpha/src/main/java/lapus/arrayx.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lapus;
+
+/**
+ *
+ * @author jaut2
+ */
+public class arrayx {
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
